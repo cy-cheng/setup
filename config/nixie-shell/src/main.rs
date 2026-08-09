3460c0ff0f7ec9f24f63b7f5361073c119d7e6a5
cy-cheng <cycheng.dev@gmail.com>
2026-08-09 23:13:13 +0800

    style: use spaced dots for clock separators

--- config/nixie-shell/src/main.rs
+++ config/nixie-shell/src/main.rs
@@ -1171,19 +1171,17 @@ fn update_ui(ui: &Ui, s: &Snapshot, update: &ModuleUpdate) {
 
 fn schedule_clock(ui: Ui) {
     let now = Local::now();
-    let small_punctuation = |value: char| {
-        format!(r#"<span font_family="TT Chocolates Trl ExtraLight" size="70%">{value}</span>"#)
-    };
+    let small_dot = r#"<span font_family="TT Chocolates Trl ExtraLight" size="70%">  .</span>"#;
     ui.clock_time.set_markup(&format!(
         "{}{}{}",
         now.format("%H"),
-        small_punctuation(':'),
+        small_dot,
         now.format("%M")
     ));
     ui.clock_date.set_markup(&format!(
         "{}{}{}",
         now.format("%m"),
-        small_punctuation('.'),
+        small_dot,
         now.format("%d")
     ));
     let delay = Duration::from_secs((60 - now.second() as u64).max(1));
